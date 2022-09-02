9b61b9c878f27df551086b7f8bd47b8fc6939f71
Rohan-sinha1209 <61308450+Rohan-sinha1209@users.noreply.github.com>
2022-09-03 03:22:07 +0530

    Add files via upload

---
+++ Publicroutelist.js
@@ -0,0 +1,12 @@
+
+import Checkout from '../components/frontend/Checkout';
+
+
+const publicRoutesList = [
+   
+ 
+    { path: '/checkout', exact: true, name: 'Checkout', component: Checkout },
+   
+];
+
+export default publicRoutesList;
